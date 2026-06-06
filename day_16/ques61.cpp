f0edbe891de4fbb5b5f2c77ca2f7d34d6fbb33eb
yogita-GLBITM <25113cn283@glbitm.ac.in>
2026-06-06 13:16:51 +0530

    Remove unwanted folders

--- day_16/ques61.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int ArrayDisplay(int arr[],int n){
-    cout<<"ENTER THE ELEMENTS OF ARRAY:";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    return 0;
-}
